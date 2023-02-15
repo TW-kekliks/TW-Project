80a033ac6c0552f966e1caf38c9b76c48932b73d
cruasanya <98823717+cruasanya@users.noreply.github.com>
2023-02-16 00:50:26 +0300

    Add project files.

---
+++ eUseControl/App_Start/RouteConfig.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace eUseControl
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                          
+            );
+            routes.MapRoute(
+                name: "ContactUs",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Contact", id = UrlParameter.Optional }
+
+            );
+            routes.MapRoute(
+                name: "about",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "about", id = UrlParameter.Optional }
+
+            );
+            routes.MapRoute(
+                name: "service",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "service", id = UrlParameter.Optional }
+
+            );
+            routes.MapRoute(
+                name: "appointment",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "appointment", id = UrlParameter.Optional }
+
+            );
+            routes.MapRoute(
+                name: "price",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "price", id = UrlParameter.Optional }
+
+            );
+            routes.MapRoute(
+                name: "about",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "about", id = UrlParameter.Optional }
+
+            );
+        }
+    }
+}
