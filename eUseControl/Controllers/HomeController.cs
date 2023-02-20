fa671cc532aa7a29104bd742c7d2214318a36372
socolovmaxim <maxim.socolov@isa.utm.md>
2023-02-20 14:16:21 +0200

    test

--- eUseControl/Controllers/HomeController.cs
+++ eUseControl/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace eUseControl.Controllers
         {
             return View();
         }
+        public ActionResult service()
+        {
+            return View();
+        }
     }
 }
