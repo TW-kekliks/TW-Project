e60c177be38ccdc62a0604a4d9ad7de1f74b87f7
cruasanya <98823717+cruasanya@users.noreply.github.com>
2023-03-13 19:08:21 +0300

    teacher

---
+++ eUseControl.BusinessLogic/SessionBL.cs
@@ -0,0 +1,18 @@
+﻿using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities.Responces;
+using eUseControl.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eUseControl.BusinessLogic
+{
+    public class SessionBL:UserApi, ISession
+    {
+        public RequestResponceAction UserLogin(ULoginData data) 
+        {
+            return UserLoginAction(data);
+        }
+    }
+}
