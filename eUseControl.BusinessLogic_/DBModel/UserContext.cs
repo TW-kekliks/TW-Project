36cc6f28c1886e0cf6e0651f9ded5fbeb1db2cbd
SomovOleg <somov675@gmail.com>
2023-04-03 19:35:02 +0300

    BusinessLogic to .NET Framework

---
+++ eUseControl.BusinessLogic_/DBModel/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Common;
+using System.Data.Entity;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    internal class UserContext : DbContext
+    {
+    }
+}
