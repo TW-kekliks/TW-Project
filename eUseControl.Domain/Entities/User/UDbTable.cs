bae2c0dc19d4f590114d0c15412d845be6a78f75
SomovOleg <somov675@gmail.com>
2023-04-03 19:24:10 +0300

    Update UDbtable

--- eUseControl.Domain/Entities/User/UDbTable.cs
+++ eUseControl.Domain/Entities/User/UDbTable.cs
@@ -10,7 +10,7 @@ namespace eUseControl.Domain.Entities.User
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         int Id { get; set; }
-        [Required]
+
         [Display(Name = "Username")]
         [StringLength(30, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 30 characters")]
         public string Username { get; set; }
@@ -21,5 +21,20 @@ namespace eUseControl.Domain.Entities.User
         [StringLength(50, MinimumLength = 8, ErrorMessage = "Password ccont be shorter than 8 characters")]
 
         public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Email address")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Email cannot be shorter than 5 characters")]
+
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+
+        public DateTime LastLogin { get; set; }
+
+        [StringLength(30)]
+        public string LastIp { get; set; } 
+        
+        public URole Level { get; set; }
     }
 }
