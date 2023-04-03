090770ff52a8c92263e0695e06e906b043b241b1
SomovOleg <somov675@gmail.com>
2023-04-03 19:19:42 +0300

    UdBTable

--- eUseControl.Domain/Entities/User/UDbTable.cs
+++ eUseControl.Domain/Entities/User/UDbTable.cs
@@ -1,13 +1,25 @@
 ﻿using System;
-using System;
+using System.ComponentModel.DataAnnotations;
 using eUseControl.Domain.Enums;
-using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eUseControl.Domain.Entities.User
 {
     public class UDbTable
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         int Id { get; set; }
-        string Name { get; set; }
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 30 characters")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password ccont be shorter than 8 characters")]
+
+        public string Password { get; set; }
     }
 }
